1b1216062e2a404223ab9ecfa2ff6fe256f39ba9
cristianomatos <cristianobmatos@gmail.com>
2014-12-29 15:58:29 -0200

    AOKP animations: Add and entry for TRANSIT_TASK_OPEN_BEHIND
    Already in frameworks/base. Option was just missing from Settings
    
    Change-Id: Ifa2af8355387b414cf8f0ddcbb1f3facb0a9173b
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/benzo/settings/animation/AnimationControls.java
+++ src/com/benzo/settings/animation/AnimationControls.java
@@ -52,6 +52,7 @@ public class AnimationControls extends SettingsPreferenceFragment implements OnP
     private static final String WALLPAPER_CLOSE = "wallpaper_close";
     private static final String WALLPAPER_INTRA_OPEN = "wallpaper_intra_open";
     private static final String WALLPAPER_INTRA_CLOSE = "wallpaper_intra_close";
+    private static final String TASK_OPEN_BEHIND = "task_open_behind";
 
     ListPreference mActivityOpenPref;
     ListPreference mActivityClosePref;
@@ -63,6 +64,7 @@ public class AnimationControls extends SettingsPreferenceFragment implements OnP
     ListPreference mWallpaperClose;
     ListPreference mWallpaperIntraOpen;
     ListPreference mWallpaperIntraClose;
+    ListPreference mTaskOpenBehind;
     SwitchPreference mAnimNoOverride;
 
     private int[] mAnimations;
@@ -156,6 +158,12 @@ public class AnimationControls extends SettingsPreferenceFragment implements OnP
         mWallpaperIntraClose.setSummary(getProperSummary(mWallpaperIntraClose));
         mWallpaperIntraClose.setEntries(mAnimationsStrings);
         mWallpaperIntraClose.setEntryValues(mAnimationsNum);
+
+        mTaskOpenBehind = (ListPreference) findPreference(TASK_OPEN_BEHIND);
+        mTaskOpenBehind.setOnPreferenceChangeListener(this);
+        mTaskOpenBehind.setSummary(getProperSummary(mTaskOpenBehind));
+        mTaskOpenBehind.setEntries(mAnimationsStrings);
+        mTaskOpenBehind.setEntryValues(mAnimationsNum);
     }
 
     //@Override
@@ -213,6 +221,10 @@ public class AnimationControls extends SettingsPreferenceFragment implements OnP
             int val = Integer.parseInt((String) newValue);
             result = Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.ACTIVITY_ANIMATION_CONTROLS[9], val);
+        } else if (preference == mTaskOpenBehind) {
+            int val = Integer.parseInt((String) newValue);
+            result = Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.ACTIVITY_ANIMATION_CONTROLS[10], val);
         }
         preference.setSummary(getProperSummary(preference));
         return result;
@@ -240,6 +252,8 @@ public class AnimationControls extends SettingsPreferenceFragment implements OnP
             mString = Settings.System.ACTIVITY_ANIMATION_CONTROLS[8];
         } else if (preference == mWallpaperIntraClose) {
             mString = Settings.System.ACTIVITY_ANIMATION_CONTROLS[9];
+        } else if (preference == mTaskOpenBehind) {
+            mString = Settings.System.ACTIVITY_ANIMATION_CONTROLS[10];
         }
 
         int mNum = Settings.System.getInt(mContext.getContentResolver(), mString, 0);
