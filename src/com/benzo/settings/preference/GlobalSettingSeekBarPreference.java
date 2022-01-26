278a05042936ac74a5e957e63dbfad6156b20254
Jason Edson <jaysonedson@gmail.com>
2022-01-25 16:16:06 -0800

    Clean up custom seekbar preference
    Fix the colors and change the icons. Clean up the preference
    code too.
    
    Change-Id: I8806f5554728cd0c7cef80061e67a9ff60fbe19e
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- src/com/benzo/settings/preference/GlobalSettingSeekBarPreference.java
+++ src/com/benzo/settings/preference/GlobalSettingSeekBarPreference.java
@@ -19,7 +19,7 @@ package com.benzo.settings.preference;
 import android.content.Context;
 import android.util.AttributeSet;
 
-public class GlobalSettingSeekBarPreference extends SeekBarPreferenceCham {
+public class GlobalSettingSeekBarPreference extends SeekBarCustomPreference {
 
     public GlobalSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
